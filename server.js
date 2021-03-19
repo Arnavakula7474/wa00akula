3e7cca4e830598d48912b8ce2eaf1d2d7d72c2ce
Arnavakula7474 <77798904+Arnavakula7474@users.noreply.github.com>
2021-03-19 11:19:23 -0500

    Created the local repo

---
+++ server.js
@@ -0,0 +1,18 @@
+const http = require('http')    //Pull in a useful node package
+                                //Try http.
+const hostname = '127.0.0.1'    //Local host
+const port = 3001               //Not assigned
+
+const wa08akula =
+  http.createServer(            //Creates the response loop
+    (req,res)=> {               //Anonymous function to handle the request
+      res.statusCode = 200      //code for OK
+      res.setHeader('Content-Type', 'text/plain') //Set the mime type
+      res.write(`I am a fitness freak. \n`)
+      res.end('Hello World')    //Close the response and provide content
+    }                           //No return needed, we modified the res object we got
+)
+
+wa08akula.listen(port, hostname, () => {   //Start the server
+  console.log(`Server running at http://${hostname}:${port}/`)  //Log the request
+})
